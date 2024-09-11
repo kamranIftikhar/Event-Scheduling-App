614a362bad053df94304c9f807989416dccf68b7
kamran-Techsurge <kamran.iftikhar@techsurgeinc.com>
2024-09-11 10:09:47 +0500

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventScheduler.Models;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using System;
+using System.IO;
+using System.Text; 
+
+namespace EventScheduler.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var calendarEvent = new CalendarEvent
+                {
+                    Summary = model.Title,
+                    Description = model.Description,
+                    Start = new CalDateTime(model.StartTime),
+                    End = new CalDateTime(model.EndTime),
+                    Location = model.Location,
+                    Uid = Guid.NewGuid().ToString()
+                };
+
+                var calendar = new Ical.Net.Calendar();
+                calendar.Events.Add(calendarEvent);
+
+                var serializer = new CalendarSerializer();
+                var serializedCalendar = serializer.SerializeToString(calendar);
+
+                var byteArray = Encoding.UTF8.GetBytes(serializedCalendar);
+                var stream = new MemoryStream(byteArray);
+
+                return File(stream, "text/calendar", "event.ics");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/EventModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventScheduler.Models
+{
+    public class EventModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        public string Location { get; set; }
+    }
+}
